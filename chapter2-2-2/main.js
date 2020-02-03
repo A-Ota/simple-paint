37ace99341946c9418438c2e6dc66afa7a279c08
太田敦士 <atsushi-ota@united.jp>
2020-02-03 14:12:19 +0900

    2-3-1追加、とリファクタリング。

--- chapter2-2-2/main.js
+++ chapter2-2-2/main.js
@@ -1,12 +1,12 @@
 // 16x16のセルを作成し配列に格納されている色を描画する
 let canvas = null
 let drawColorInput = null
-const CELL_SIZE = 16
-const CELL_NUM = 16
+const PIXEL_SIZE = 16
+const CANVAS_SIZE = 16
 // 16x16のピクセルを格納する2次元配列
-const cellData = new Array(CELL_NUM)
-for (let i = 0; i < CELL_NUM; ++i) {
-  cellData[i] = new Array(CELL_NUM).fill('cccccc')
+const pixelData = new Array(PIXEL_NUM)
+for (let i = 0; i < PIXEL_NUM; ++i) {
+  pixelData[i] = new Array(PIXEL_NUM).fill('cccccc')
 }
 
 // クリック時にカラーピッカーで選択されている色を出力する。
@@ -23,10 +23,10 @@ function init() {
 function drawCanvas() {
   const ctx = canvas.getContext('2d')
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  for (let y = 0; y < CELL_NUM; ++y) {
-    for (let x = 0; x < CELL_NUM; ++x) {
-      ctx.fillStyle = '#' + cellData[x][y]
-      ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)      
+  for (let y = 0; y < PIXEL_NUM; ++y) {
+    for (let x = 0; x < PIXEL_NUM; ++x) {
+      ctx.fillStyle = '#' + pixelData[x][y]
+      ctx.fillRect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE)      
     }
   }
 }
--- chapter2-2-3/main.js
+++ chapter2-2-3/main.js
@@ -1,12 +1,12 @@
 // 16x16のセルを作成し配列に格納されている色を描画する
 let canvas = null
 let drawColorInput = null
-const CELL_SIZE = 16
-const CELL_NUM = 16
+const PIXEL_SIZE = 16
+const CANVAS_SIZE = 16
 // 16x16のピクセルを格納する2次元配列
-const cellData = new Array(CELL_NUM)
-for (let i = 0; i < CELL_NUM; ++i) {
-  cellData[i] = new Array(CELL_NUM).fill('cccccc')
+const pixelData = new Array(PIXEL_NUM)
+for (let i = 0; i < PIXEL_NUM; ++i) {
+  pixelData[i] = new Array(PIXEL_NUM).fill('cccccc')
 }
 
 // クリック時にカラーピッカーで選択されている色を出力する。
@@ -14,9 +14,9 @@ function init() {
   const drawColorInput = document.getElementById('draw-color')
   canvas = document.getElementById('main-canvas')
   canvas.addEventListener('click', event => {
-    const cellX = Math.floor(event.offsetX / CELL_SIZE)
-    const cellY = Math.floor(event.offsetY / CELL_SIZE)
-    cellData[cellX][cellY] = drawColorInput.value.replace('#', '')
+    const pixelX = Math.floor(event.offsetX / PIXEL_SIZE)
+    const pixelY = Math.floor(event.offsetY / PIXEL_SIZE)
+    pixelData[pixelX][pixelY] = drawColorInput.value.replace('#', '')
     drawCanvas()
   })
   drawCanvas()
@@ -26,10 +26,10 @@ function init() {
 function drawCanvas() {
   const ctx = canvas.getContext('2d')
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  for (let y = 0; y < CELL_NUM; ++y) {
-    for (let x = 0; x < CELL_NUM; ++x) {
-      ctx.fillStyle = '#' + cellData[x][y]
-      ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)      
+  for (let y = 0; y < PIXEL_NUM; ++y) {
+    for (let x = 0; x < PIXEL_NUM; ++x) {
+      ctx.fillStyle = '#' + pixelData[x][y]
+      ctx.fillRect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE)      
     }
   }
 }
