325f018858b69fa3fb4e3f56847885a36bd40cb7
太田敦士 <atsushi-ota@united.jp>
2020-02-04 17:20:46 +0900

    2-5-1追加。

---
+++ chapter2-5-1/plugin.js
@@ -0,0 +1,17 @@
+// プラグイン製作者に提供するインターフェース群
+function getCanvasSize() {
+  return CANVAS_SIZE
+}
+
+function getPixelColor(x, y) {
+  return pixelData[x][y]
+}
+
+function setPixelColor(x, y, color) {
+  pixelData[x][y] = color
+  drawCanvas()
+}
+
+function setPixelCallback(callback) {
+  pixelCallback = callback
+}
